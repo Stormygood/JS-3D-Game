d50f750d516387ed5a59cae9c618f95dfb58a5e9
Stormygood <bgbgundergamesbgbg789@gmail.com>
2026-07-14 17:51:25 +0300

    added math coordinates to dx and dy in teh css file

--- kunnin.js
+++ kunnin.js
@@ -1,3 +1,7 @@
+//world const
+var deg = Math.PI/180;
+
+
 //plrs class
 
 function player(x, y, z, rx, ry){
@@ -24,7 +28,8 @@ var Rotate = 0;
 var speed = 1;
 //tp var to check if it has been teleported already or not
 let tp_var = 0;
-
+var mouseX = 0;
+var mouseY = 0;
 
 
 //if the key is pressed
@@ -32,20 +37,23 @@ document.addEventListener("keydown", (event) => {
     if(event.key == 'r'){
     nob.x = 0;
     nob.y = 0;
+    nob.z = 0;
+    nob.rx = 0;
+    nob.ry = 0;
     console.log("TPd center")
     }
 
     if(event.key == 't'){
         if(tp_var == 1){
-        nob.x = ork.scrollWidth * (-0.5);
-        nob.y = ork.scrollHeight * (-0.5);  
+        nob.x = -400;
+        nob.y = -300;  
         tp_var = 0;  
         console.log("TPd opposite");
         console.log(nob.x);
         console.log(nob.y);
         } else {
-            nob.x = ork.clientWidth * 0.5;
-        nob.y = ork.clientHeight * 0.5;
+            nob.x = 400;
+        nob.y = 300;
         tp_var = 1;
         console.log("TPd new");
         console.log(nob.x);
@@ -107,23 +115,40 @@ document.addEventListener("keyup", (event) => {
     }
 })
 
+document.addEventListener("mousemove", (event) => {
+    mouseX = event.movementX;
+    mouseY = event.movementY;
+})
+
 
 
 function update(){
     // 1st count mvmnt
-    dx = PressRight - PressLeft;
-    dz = PressForward - PressBack;
+    dx = (PressRight - PressLeft) * Math.cos(nob.ry * deg) - (PressForward - PressBack) * Math.sin(nob.ry * deg);
+    dz = -((PressForward - PressBack) * Math.sin(nob.ry * deg) - (PressRight - PressLeft) * Math.cos(nob.ry * deg));
     dy = -PressUp;
-    // console.log(PressRight);
+
+    drx = mouseY;
+    dry = mouseX;
+    mouseX = mouseY = 0;
+    console.log(nob.x, nob.y, nob.z, nob.rx, nob.ry);
 
     //add mvmnt to the coordinates
     //speed is multiplied to the differnece of the coordinates, so the change of the coordinates is sped up
+    //chng coords of Ork
     nob.x = nob.x + dx * speed;
     nob.y = nob.y + dy * speed;
     nob.z = nob.z + dz * speed;
-    //chng coords of Ork
 
-    ork.style.transform = "translate3d(" + (-nob.x) + "px, " + (-nob.y) + "px," + (-nob.z) + "px)";
+
+    //add lookng around
+
+    nob.rx = nob.rx +  drx;
+    nob.ry = nob.ry + dry;
+
+    ork.style.transform = "translateZ(100px)" + "rotateX(" + (-nob.rx) +"deg)"+
+    "rotateY(" + (-nob.ry) + "deg)"+
+    "translate3d(" + (-nob.x) + "px, " + (-nob.y) + "px," + (-nob.z) + "px)";
 
 
 }
